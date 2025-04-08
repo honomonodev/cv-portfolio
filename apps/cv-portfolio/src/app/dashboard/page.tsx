9b3cad341e08af8193bb0a2fb231986998519a2d
GitRockie <150247498+honomonodev@users.noreply.github.com>
2025-04-08 23:54:59 +0200

    restructuring clean

---
+++ apps/cv-portfolio/src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+export default function DashboardPage() {
+  return (
+    <section className="max-w-7xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4">Dashboard</h1>
+      <p className="text-gray-700 dark:text-gray-300">
+        Welcome to your Dashboard. Here you will manage workspaces, schedules,
+        and life commits!
+      </p>
+    </section>
+  );
+}
